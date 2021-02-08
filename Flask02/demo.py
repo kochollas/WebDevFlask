b2cfa8af9a1431af0c10fee095a362b57544c2e9
Kochola <mike@hivisasa.com>
2021-02-08 12:47:39 +0300

    second assignment

---
+++ Flask02/demo.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/about', methods = ['GET'])
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(port= 7000, debug=True)
